e6c0a1c0ab105d1c90a3ce9c6db928e3c4a0120a
Yong-Hae Lee <sundlee@gmail.com>
2020-08-02 19:18:30 +0900

    Add portfolio and Review

--- portfolio.js
+++ portfolio.js
@@ -77,9 +77,122 @@ document.getElementById('secondDot').addEventListener('click', currentImageSlide
 document.getElementById('thirdDot').addEventListener('click', currentImageSlide.bind(null, 3));
 document.getElementById('forthDot').addEventListener('click', currentImageSlide.bind(null, 4));
 
+/* portfolio area */
+filterSelection('all');
 
+function filterSelection(id) {
+  var x, i;
 
+  x = document.getElementsByClassName('listItem');
+  for (i=0; i < x.length; i++) {
+    removeClass(x[i], 'active');
+  }
+  addClass(document.getElementById(id), 'active');
 
+  x = document.getElementsByClassName('filterItem');
+  if (id == 'all') id = '';
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], 'show');
+    if (x[i].className.indexOf(id) > -1) {
+      addClass(x[i], 'show');
+    }
+  }
+}
 
+function addClass(element, name) {
+  if (element.className.indexOf(name) == -1) {
+    element.className += " " + name;
+  }
+}
 
+function removeClass(element, name) {
+  var arr;
+  arr = element.className.split(" ");
 
+  while (arr.indexOf(name) > -1) {
+    arr.splice(arr.indexOf(name), 1);
+  }
+
+  element.className = arr.join(" ");
+}
+
+document.getElementById('all').addEventListener('click', filterSelection.bind(null, 'all'));
+document.getElementById('uiux').addEventListener('click', filterSelection.bind(null, 'uiux'));
+document.getElementById('java').addEventListener('click', filterSelection.bind(null, 'java'));
+document.getElementById('db').addEventListener('click', filterSelection.bind(null, 'db'));
+
+function viewPortfolio(event) {
+  var polyNode = event.target;
+
+  if (polyNode.tagName.toLowerCase() =='i') { polyNode = polyNode.parentNode; }
+
+  var overlayNode = polyNode;
+  var imageNode = overlayNode.nextElementSibling;
+
+  var itemNode = overlayNode.parentNode;
+  var mainNode = itemNode.nextElementSibling;
+  var subNode = mainNode.nextElementSibling;
+  var textNode = subNode.nextElementSibling;
+
+  document.getElementById('modalImage').src = imageNode.src;
+  document.getElementById('modalMain').innerHTML = mainNode.innerHTML;
+  document.getElementById('modalSub').innerHTML = subNode.innerHTML;
+  document.getElementById('modalText').innerHTML = textNode.innerHTML;
+
+  document.getElementById('portfolioModal').style.display = 'block';
+}
+
+document.getElementById('modalClose').addEventListener('click', function() {
+  document.getElementById('portfolioModal').style.display = 'none';
+})
+
+var filterItems = document.getElementsByClassName('overlay');
+
+for (var i = 0; i < filterItems.length; i++) {
+  filterItems[i].addEventListener('click', viewPortfolio);
+}
+
+/* review area */
+var reviewSlideIndex = 0;
+
+function reviewSlideTimer() {
+  plusReviewSlides(1);
+}
+
+var reviewTimer = setInterval(reviewSlideTimer, 3000);
+
+function plusReviewSlides(n) {
+  clearInterval(reviewTimer);
+  reviewTimer = setInterval(reviewSlideTimer, 3000);
+  showReviewSlides(reviewSlideIndex += n);
+}
+
+function showReviewSlides(n) {
+  var i;
+  var review_slides = document.getElementsByClassName('review-slide');
+
+  if (n > review_slides.length - 2) {
+    reviewSlideIndex = 0;
+  }
+
+  if (n < 0) {
+    reviewSlideIndex = review_slides.length - 3;
+  }
+
+  for (i = 0; i < review_slides.length; i++) {
+    removeClass(review_slides[i], 'show');
+    removeClass(review_slides[i], 'res-show');
+    addClass(review_slides[i], 'hide');
+  }
+
+  removeClass(review_slides[reviewSlideIndex], 'hide');
+  addClass(review_slides[reviewSlideIndex], 'res-show');
+
+  removeClass(review_slides[reviewSlideIndex + 1], 'hide');
+  addClass(review_slides[reviewSlideIndex + 1], 'show');
+  removeClass(review_slides[reviewSlideIndex + 2], 'hide');
+  addClass(review_slides[reviewSlideIndex + 2], 'show');
+}
+
+document.getElementById('reviewPrev').addEventListener('click', plusReviewSlides.bind(null, -1));
+document.getElementById('reviewNext').addEventListener('click', plusReviewSlides.bind(null, 1));
